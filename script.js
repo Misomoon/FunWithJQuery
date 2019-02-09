a1470553f95b3e4cc80588543a8290a16e2ba960
student <student@Admins-MBP.attlocal.net>
2019-02-08 18:27:01 -0800

    Updated

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
 $(document).ready(function(){
 // all jQuery methods go here...
+$("#runningman-img").hover(enter, leave);
+function enter(){
+  this.src="images/runningman-giphy.gif"
+}
 
+function leave(){
+  this.src = "images/runningman-icon.png"
 
+}
+$("#taketheL-img").hover(enter2, leave2);
+function enter2(){
+  this.src="images/taketheL-giphy.gif"
+}
 
+function leave2(){
+  this.src = "images/taketheL-icon.png"
 
+}
+$("#electroshuffle-img").hover(enter3, leave3);
+function enter3(){
+  this.src="images/electroshuffle-giphy.gif"
+}
 
+function leave3(){
+  this.src = "images/electroshuffle-icon.png"
 
+}
+$("#bestmates-img").hover(enter4, leave4);
+function enter4(){
+  this.src="images/bestmates-giphy.gif"
+}
 
+function leave4(){
+  this.src = "images/bestmates-icon.png"
+
+}
+$(function(){
+  $(document).attr("title","Emote Information")
+})
+$("#title").html("This is Emote Information")
+$("#extra-text").remove()
+
+$("#logo").click(bgImageChange);
+function bgImageChange(){
+$("body").css("background-image", "url('images/bg2.jpg')");
+}
 
 
 
